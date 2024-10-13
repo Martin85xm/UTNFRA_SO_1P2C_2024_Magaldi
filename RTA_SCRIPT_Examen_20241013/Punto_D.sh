f48a76e7d3e3c61c5f9bfd423654c31a4e3f096a
Martin85xm <mmolero1985@gmail.com>
2024-10-13 02:05:40 +0000

    add: Punto_D resuelto

---
+++ RTA_SCRIPT_Examen_20241013/Punto_D.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Creo las carpetas sin utlizar sudo ya que no estamos en la carpeta raiz
+mkdir -p /home/vagrant/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},cliente/cartas_{1..100}}
